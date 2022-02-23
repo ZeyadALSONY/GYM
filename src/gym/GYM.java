273860a25219e84266845176d2298ec3340e2f72
ZeyadALSONY <100289148+ZeyadALSONY@users.noreply.github.com>
2022-02-23 16:22:15 +0200

    Initial commit

---
+++ src/gym/GYM.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gym;
+
+/**
+ *
+ * @author spider
+ */
+public class GYM {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        plogin g1 =new plogin();
+        g1.setVisible(true);
+        g1.setLocation(150,150);
+       
+        
+    }
+    
+}
---
+++ src/gym/image1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gym;
+
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author spider
+ */
+public class image1 extends JPanel { 
+    private ImageIcon i;
+    public  image1(){
+        this.setLayout(null);
+        
+    }
+    
+    
+}
